eb5ca181b83530cf0a32801956b3aaba042db2e6
Nitul Das <ndas6732@gmail.com>
2025-02-27 23:42:10 +0530

    Fix CORS configuration by removing trailing slash from allowed origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://astitvafoundation.vercel.app/",
+            "https://astitvafoundation.vercel.app",
         ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
