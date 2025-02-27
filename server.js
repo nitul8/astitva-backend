6d58a05beacf347f9c38dd80ec56997734ff41a0
Nitul Das <ndas6732@gmail.com>
2025-02-27 23:18:36 +0530

    Load environment variables using dotenv in server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const userSchema = new mongoose.Schema(
     {timestamps: true}
 );
 
-require("dotenv").config(); // Load environment variables
+require("dotenv").config();
 mongoose
     .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
